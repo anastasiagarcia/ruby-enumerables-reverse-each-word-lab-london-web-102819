6c1b2d37498f565a31a48ff2048c721b4bc6b8f5
Anastasia Pankina <siapankina@gmail.com>
2019-10-16 14:50:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,14 @@
-def reverse_each_word(sentence)
-  reversed = sentence.split(" ").collect do |word|
-  word.reverse
+
+def reverse_each_word(string)
+  array = string.split(" ") 
+  test_array = []
+  array.collect do|string| 
+    test_array << string.reverse 
+  end
+  test_array.join(" ")
 end
-  reversed.join(" ")
-end
+
+
+reverse_each_word("Hello there, and how are you?")
+  #=> "olleH ,ereht dna woh era ?uoy"
+  
